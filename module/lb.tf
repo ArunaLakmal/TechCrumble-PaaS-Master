de1f450c876e1bf5cc207e31b4a48bf804b85d93
Aruna Lakmal <arunarandunu@gmail.com>
2020-10-12 11:12:29 +0530

    dependencies and listener changes

--- module/lb.tf
+++ module/lb.tf
@@ -19,6 +19,20 @@ resource "aws_lb_target_group" "int_kube_master_tg" {
   vpc_id   = "${var.vpc_id}"
 }
 
+resource "aws_lb_target_group" "int_kube_master_6443" {
+  name     = "int-kube-master-6443"
+  port     = 6443
+  protocol = "TCP"
+  vpc_id   = "${var.vpc_id}"
+}
+
+resource "aws_lb_target_group" "int_kube_master_443" {
+  name     = "int-kube-master-443"
+  port     = 443
+  protocol = "TCP"
+  vpc_id   = "${var.vpc_id}"
+}
+
 resource "aws_lb_listener" "int_kube_master_listener" {
     load_balancer_arn = "${aws_lb.int_kube_master_lb.arn}"
     port = "80"
@@ -30,8 +44,30 @@ resource "aws_lb_listener" "int_kube_master_listener" {
   }
 }
 
+resource "aws_lb_listener" "int_kube_master_listener_6443" {
+    load_balancer_arn = "${aws_lb.int_kube_master_lb.arn}"
+    port = "6443"
+    protocol = "TCP"
+
+    default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.int_kube_master_6443.arn}"
+  }
+}
+
+resource "aws_lb_listener" "int_kube_master_listener_443" {
+    load_balancer_arn = "${aws_lb.int_kube_master_lb.arn}"
+    port = "443"
+    protocol = "TCP"
+
+    default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.int_kube_master_443.arn}"
+  }
+}
+
 resource "aws_autoscaling_attachment" "int_lb-kube_master_attachment" {
   autoscaling_group_name = "${aws_autoscaling_group.kube_master_asg.id}"
   alb_target_group_arn = "${aws_lb_target_group.int_kube_master_tg.id}"
-  depends_on = ["${aws_lb.int_kube_master_lb}"] 
+  depends_on = [aws_lb.int_kube_master_lb] 
 }
